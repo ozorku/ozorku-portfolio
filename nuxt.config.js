4698ae9c66f9e6a511370d8b1e9d2b99a44de767
adavize <adavize@thepoint.africa>
2020-03-22 18:54:29 +0100

    update meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,51 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
+        name: "author",
+        content: "Adavize Ozorku"
+      },
+      {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || ""
+        content:
+          "Adavize Ozorku is a Software Developer currently based in Lagos, Nigeria"
+      },
+      // facebook
+      {
+        property: "og:site_name",
+        content: "Adavize Ozorku"
+      },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://ozorku.dev" },
+      {
+        property: "og:image",
+        content: "images/adavize-ozorku-developer-card.png"
+      },
+      {
+        property: "og:description",
+        content: "Software Developer based in Lagos, Nigeria"
+      },
+      {
+        property: "og:title",
+        content: "Software Developer based in Lagos, Nigeria - Adavize Ozorku"
+      },
+      // twitter
+      { name: "twitter:domain", content: "ozorku.dev" },
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:site", content: "@ozorku" },
+      { name: "twitter:creator", content: "@ozorku" },
+      { name: "twitter:url", content: "https://ozorku.dev" },
+      {
+        name: "twitter:title",
+        content: "Adavize Ozorku - Software Developer based in Lagos"
+      },
+      {
+        name: "twitter:description",
+        content: "Software Developer based in Lagos, Nigeria."
+      },
+      {
+        name: "twitter:image",
+        content: "https://ozorku.com/images/adavize-ozorku-developer-card.png"
       }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }]
